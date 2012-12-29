262bcd53ecfb435109f74dcf783164266bbb00e4
gyz <fack@fake.com>
2012-12-29 11:40:03 +0800

    test commit

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(int argc, const char* argv[]) {
+	cout << "only a test" << endl;
+	cout << "version 0.0.2" << endl;
+	cout << "test" << endl;
+	return 0
+}
